08c2da3eb2fddd4b8d2e56ded97d71325feefac3
paparazzit <paparazzit@fgmail.com>
2021-03-05 16:09:31 +0100

    ION gotov

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		margin: 80,
+		nav: false,
+		dots: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplaySpeed: 1000,
+
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 2,
+			},
+			1000: {
+				items: 2,
+			},
+		},
+	});
+});
